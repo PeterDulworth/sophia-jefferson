3d9bf9403962882518d484d1e56123b68154984b
Peter Dulworth <pdulworth97@gmail.com>
2019-08-12 22:58:55 -0500

    Updates

---
+++ precache-manifest.5ecb52c666e16807ea92f9e9153cab1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f947fc73297d8b5144b367603bb61b6",
+    "url": "/sophia-jefferson/index.html"
+  },
+  {
+    "revision": "59ee1d9638a7d96b5f14",
+    "url": "/sophia-jefferson/static/css/main.fdd9db67.chunk.css"
+  },
+  {
+    "revision": "47eaf4f92d82170ef3a6",
+    "url": "/sophia-jefferson/static/js/2.818de4a2.chunk.js"
+  },
+  {
+    "revision": "59ee1d9638a7d96b5f14",
+    "url": "/sophia-jefferson/static/js/main.e9b830e6.chunk.js"
+  },
+  {
+    "revision": "59a391493c52c7362a06",
+    "url": "/sophia-jefferson/static/js/runtime~main.628b2931.js"
+  },
+  {
+    "revision": "adf9dc5f2db65b6983048f4b17a9cee9",
+    "url": "/sophia-jefferson/static/media/bike.adf9dc5f.JPG"
+  },
+  {
+    "revision": "7503ca73c283e3e8fc6a8b348076b601",
+    "url": "/sophia-jefferson/static/media/hannah.7503ca73.JPG"
+  },
+  {
+    "revision": "d26d8db670b3012f7f9ce541c6ca9739",
+    "url": "/sophia-jefferson/static/media/silly.d26d8db6.JPG"
+  },
+  {
+    "revision": "a2ca14f724c33e0c0acdffd0f29a1ec2",
+    "url": "/sophia-jefferson/static/media/soph.a2ca14f7.JPG"
+  }
+]);
