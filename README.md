ef6adc8ea862bb0c2b18de74fc9866a44addd8a6
Peter Dulworth <pdulworth97@gmail.com>
2023-03-22 11:00:14 -0700

    update dependencies / redeploy

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 ## Deploy
 
 #### deploying to sophiajefferson.com
-to deploy to sophiajefferson.com, change the "homepage" field in `package.json` to 
-```javascript
+to deploy to [sophiajefferson.com](http://sophiajefferson.com/), change the `homepage` field in `package.json` to 
+```bash
 "homepage": "http://sophiajefferson.com/",
 ```
 and run
 ```shell script
 npm run build
 ```
-then copy the contents of the `build` directory to the server.
+then copy the contents of the `build` directory to the server (`/public_html/additional_websites/sophiajefferson.com`).
 
 #### deploying to github pages
-to deploy to peterdulworth.github.io/sophia-jefferson, change the "homepage" field in `package.json` to 
-```javascript
+to deploy to [peterdulworth.github.io/sophia-jefferson](https://peterdulworth.github.io/sophia-jefferson/), change the `homepage` field in `package.json` to 
+```bash
 "homepage": "https://peterdulworth.github.io/sophia-jefferson",
 ```
 and run
@@ -21,3 +21,10 @@ and run
 npm run deploy
 ```
 this will push to the gh-pages branch and auto deploy
+
+## Local Dev
+
+```bash
+npm install
+npm run start
+```
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <div className="Footer">
-            <div className="Footer__copy">
-                &copy; 2019 Peter Dulworth
-            </div>
-            <div className="Footer__contact">
-                Please send pictures of your <span role="img" aria-label="dog">🐕</span> to sgj1@rice.edu.
-            </div>
-                <div className="Footer__social">
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sophia-j-b9097a129/">LinkedIn</a>
-                    <a target="_blank" rel="noopener noreferrer" href="http://sophiajefferson.com/resume/sophiajefferson.pdf">Resume</a>
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/isticatedsoph/">Instagram</a>
-                </div>
-        </div>
-    );
+  return (
+    <div className='Footer'>
+      <div className='Footer__copy'>
+        <a target='_blank' rel='noopener noreferrer' href='http://sophiajefferson.com/resume/sophiajefferson.pdf'>
+          Resume
+        </a>
+      </div>
+      <div className='Footer__contact'>
+        Please send pictures of your{' '}
+        <span role='img' aria-label='dog'>
+          🐕
+        </span>{' '}
+        to sgj1@rice.edu.
+      </div>
+      <div className='Footer__social'>
+        <a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/sophia-j-b9097a129/'>
+          LinkedIn
+        </a>
+        <a target='_blank' rel='noopener noreferrer' href='https://www.instagram.com/isticatedsoph/'>
+          Instagram
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
